24d4496d32b04e3978c0bc1e387f4b66b9624f92
Uyennn25 <tranhunguyen4@gmail.com>
2024-09-13 15:36:55 +0700

    Done

--- Super Mario Bros/Assets/GameTool/Obsever/EventID.cs
+++
@@ -1,7 +0,0 @@
-public enum EventID
-{
-    None = 0,
-    OnProtect,
-    OnChangePlayer,
-    OpenDoor,
-}
--- Super Mario Bros/Assets/_GameMario/Scripts/UI/MenuGameplay.cs
+++
@@ -1,9 +0,0 @@
-﻿using GameTool;
-using UnityEngine;
-
-namespace _GameMario.Scripts.UI
-{
-    public class MenuGameplay : SingletonUI<MenuGameplay>
-    {
-    }
-}
